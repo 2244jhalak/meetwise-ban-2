1f0634bb525f940f5b7802d5d0732262da838f91
Jhalak <stdjhalak@gmail.com>
2024-09-18 22:28:43 +0600

    try

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const page = () => {
     return (
         <div>
-            <h2>This is my login</h2>
+            <h2>This is my login!!</h2>
         </div>
     );
 };
